22d47b9842be6dabe35cfd18c87b4db920b94fce
Jaren Braza <brazaj1@unlv.nevada.edu>
2021-07-11 05:24:49 -0700

    Add project files.

---
+++ CSharpDataStructure/Trees/BinarySearchTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpDataStructures.Trees
+{
+	class BinarySearchTree<TValue> : ICollection<TValue>, IEnumerable<TValue>
+	{
+		/// <summary>
+		/// Gets the number of elements contained in the <see cref="BinarySearchTree{TValue}"/>.
+		/// </summary>
+		/// <returns>
+		/// The number of elements contained in the <see cref="BinarySearchTree{TValue}"/>.
+		/// </returns>
+		public int Count { get; private set; }
+
+		/// <summary>
+		/// Gets a value indicating whether the <see cref="BinarySearchTree{TValue}"/> is read-only.
+		/// </summary>
+		/// <returns>
+		/// true if the <see cref="BinarySearchTree{TValue}"/> is read-only; otherwise, false
+		/// </returns>
+		public bool IsReadOnly { get; } = false;
+
+		/// <summary>
+		/// Adds an item to the <see cref="BinarySearchTree{TValue}"/>.
+		/// </summary>
+		/// <param name="value">The object to add to the <see cref="BinarySearchTree{TValue}"/>.</param>
+		public void Add(TValue value)
+		{
+
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(TValue item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo(TValue[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerator<TValue> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Remove(TValue item)
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ CSharpDataStructure/Trees/BinaryTree.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpDataStructures
+{
+	/// <summary>
+	/// Represents a collection of binary tree nodes.
+	/// </summary>
+	/// <typeparam name="TValue">The type of the values in the binary tree.</typeparam>
+	class BinaryTree<TValue>
+	{
+		public int Count { get; private set; } = 0;
+		public bool IsReadOnly { get; } = false;
+		public BinaryTreeNode<TValue> Root { get; private set; }
+
+		public BinaryTree(BinaryTreeNode<TValue> root)
+		{
+			Root = root;
+		}
+
+		public BinaryTree(TValue value)
+		{
+			Root = new BinaryTreeNode<TValue>(value);
+		}
+	}
+
+	class BinaryTreeNode<TValue>
+	{
+		public TValue Value { get; set; }
+		public BinaryTreeNode<TValue> Left { get; set; }
+		public BinaryTreeNode<TValue> Right { get; set; }
+		public BinaryTree<TValue> Tree { get; }
+
+		public BinaryTreeNode(TValue value)
+		{
+			Value = value;
+		}
+	}
+}
